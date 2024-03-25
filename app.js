c93a1a28ba8331a36e7b5e5f7e3a6e7897a68cfc
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-03-25 16:20:48 +0500

    added

---
+++ app.js
@@ -0,0 +1,17 @@
+// let numbers = [3,5,6,2]
+// let sum =0
+// for (let i of numbers) {
+//     sum+=i  
+// }
+// console.log(sum);
+
+
+// let object={
+//     ism : "Iskandar",
+//     yosh : 17,
+//     method(){
+//         return this.ism
+//     }
+// }
+
+// console.log(object.method());
